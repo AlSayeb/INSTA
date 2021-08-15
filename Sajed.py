1ce7d1d4dc35521b87ce3e0e9f9459bbde165590
AlSayeb <88703388+AlSayeb@users.noreply.github.com>
2021-08-15 20:34:47 +0300

    Add files via upload

---
+++ Sajed.py
@@ -0,0 +1,190 @@
+#decode by - Damar
+import requests
+import uuid
+import sys
+import os
+from random import *
+import colorama 
+from colorama import *
+import time 
+import string 
+from user_agent import generate_user_agent
+import random
+import webbrowser
+import requests
+
+def wa():    webbrowser.open('https://t.me/H_ils')
+
+
+def restart():
+    ngulang = sys.executable
+    os.execl(ngulang, ngulang, *sys.argv)
+os.system('cls' if os.name == 'nt' else 'clear')
+print("""
+\033[1;96m ------------------------
+ \033[1;32m < COD BY Sajed >
+ \033[1;96m ------------------------
+ _____       ___       _   _____   _____  
+/  ___/     /   |     | | | ____| |  _  \ 
+| |___     / /| |     | | | |__   | | | | 
+\___  \   / / | |  _  | | |  __|  | | | | 
+ ___| |  / /  | | | |_| | | |___  | |_| | 
+/_____/ /_/   |_| \_____/ |_____| |_____/ 
+
+\033[1;96m ------------------------
+ \033[1;93m < FIRST STEP OF LOGIN >
+ \033[1;96m ------------------------
+\033[1;97m 
+                           """)
+
+user = input('          \033[1;94mTOOL USERNAME: ')
+sandi = input('          \033[1;93mTOOL PASSWORD: ')
+if sandi == 'Sajed' and user == 'Sajed':
+    print( 'Anda Telah Login')
+    sys.exit
+else:
+    print("""
+\033[1;96m ------------------------
+ \033[1;32m < FIRST STEP OF LOGIN >
+ \033[1;96m ------------------------
+""")
+    wa()
+    restart()
+os.system('cls' if os.name == 'nt' else 'clear')
+lib = input("""
+\033[1;96m ------------------------
+ \033[1;32m < COD BY Sajed >
+ \033[1;96m ------------------------
+ _____       ___       _   _____   _____  
+/  ___/     /   |     | | | ____| |  _  \ 
+| |___     / /| |     | | | |__   | | | | 
+\___  \   / / | |  _  | | |  __|  | | | | 
+ ___| |  / /  | | | |_| | | |___  | |_| | 
+/_____/ /_/   |_| \_____/ |_____| |_____/ 
+
+          \033[1;92m[1] DOWNLOAD & LIB UODATE
+          \033[1;92m[2] PASS
+
+\033[1;91m[+] Please Choice >>\033[1;92m
+""")
+if lib == "1":
+    os.system('pip install requests')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('pip install uuid')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('pip install colorama')
+    os.system('cls' if os.name == 'nt' else 'clear')
+    pass
+else:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    pass
+os.system('cls' if os.name == 'nt' else 'clear')
+print("""
+\033[1;91m
+ _____       ___       _   _____   _____  
+/  ___/     /   |     | | | ____| |  _  \ 
+| |___     / /| |     | | | |__   | | | | 
+\___  \   / / | |  _  | | |  __|  | | | | 
+ ___| |  / /  | | | |_| | | |___  | |_| | 
+/_____/ /_/   |_| \_____/ |_____| |_____/ 
+""")
+webbrowser.open('https://t.me/H_ils')
+tele = input('[+] Send Users To Telegram bot [Y/y] : ')
+if tele == 'y' or tele == 'Y':
+     TK = input('[+] Enter Bot Token :\033[1;97m  ')
+     ID = input('\033[1;32m [+] Enter Telegram ID : \033[1;97m ')
+     pass
+else:
+	pass
+#Sidra#
+os.system('cls' if os.name == 'nt' else 'clear')
+print("""
+\033[1;96m ------------------------
+ \033[1;32m < COD BY Sajed >
+ \033[1;96m ------------------------
+\033[1;33m
+  _____       ___       _   _____   _____  
+/  ___/     /   |     | | | ____| |  _  \ 
+| |___     / /| |     | | | |__   | | | | 
+\___  \   / / | |  _  | | |  __|  | | | | 
+ ___| |  / /  | | | |_| | | |___  | |_| | 
+/_____/ /_/   |_| \_____/ |_____| |_____/ 
+""")
+webbrowser.open('https://t.me/H_ils')
+h = '_' 
+k = '1'
+tuks1 = 'poiuytrewqasdfghjklmnbvcxz12'
+jsidra = '\033[1;33m'
+_sidra  = '\033[1;31m'
+BGreen='\033[1;32m'
+BRed ='\033[1;31m'
+#Cod by SidraELEzz #
+sidra = input("""
+\033[1;96m ------------------------
+ \033[1;32m < COD BY Sajed >
+ \033[1;96m ------------------------
+
+\033[1;97m 
+          \033[1;92m[1] START CHECKER
+          \033[1;92m[2] Follow Sajed
+
+\033[1;91m[+] Please Choice >>\033[1;92m
+""")
+ru = ('{"account_created": false, "errors": {"email": [{"message": "Too many accounts are using a@gmail.com.", "code": "email_sharing_limit"}], "__all__": [{"message": "Create a password at least 6 characters long.", "code": "too_short_password"}]}, "dryrun_passed": false, "username_suggestions": [], "status": "ok", "error_type": "form_validation_error"}')
+def Code_sidra():    
+    if sidra =='1':
+        Number = 0
+        while True:
+            user = str("".join(random.choice(tuks1)for i in range(5)))
+            us = str("".join(random.choice(h+user)for i in range(5)))            
+            url = 'https://www.instagram.com/accounts/web_create_ajax/attempt/'
+            heade_sidra={
+                'accept': '*/*',
+                'accept-encoding': 'gzip, deflate, br',
+                'accept-language': 'ar,en-US;q=0.9,en;q=0.8,ar-SA;q=0.7',
+                'content-length': '61',
+                'content-type': 'application/x-www-form-urlencoded',
+                'cookie': 'ig_cb=2; ig_did=BB52B198-B05A-424E-BA07-B15F3D4C3893; mid=YAlcaQALAAHzmX6nvD8dWMRVYFCO; shbid=15012; rur=PRN; shbts=1612894029.7666144; csrftoken=CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+                'origin': 'https://www.instagram.com',
+                'referer': 'https://www.instagram.com/accounts/emailsignup/',
+                'sec-ch-ua': '"Google Chrome";v="87", " Not;A Brand";v="99", "Chromium";v="87"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-fetch-dest': 'empty',
+                'sec-fetch-mode': 'cors',
+                'sec-fetch-site': 'same-origin',
+                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+                'x-csrftoken': 'CPKow8myeXW9AuB3Lny0wNxx0EzoDQoI',
+                'x-ig-app-id': '936619743392459',
+                'x-ig-www-claim': 'hmac.AR0Plwj5om112fwzrrYnMNjMLPnyWfFFq1tG7MCcMv5_vN9M',
+                'x-instagram-ajax': '72bda6b1d047',
+                'x-requested-with': 'XMLHttpRequest'
+
+            } 
+#The data shown above are identical Do not touch anything developer sidra          
+            data_sidra={
+                'email' : 'a@gmail.com',
+                'username': f'{us}',
+                'first_name': 'AA',
+                'opt_into_one_tap': 'false'
+            }
+#sidra elezz
+            da = requests.post(url,headers=heade_sidra,data=data_sidra).text
+            if ru in  da:
+                Number+=1
+                print(jsidra+'['+BGreen+f'{Number}'+jsidra+'] Live username'+BGreen+'->'+jsidra+'['+BGreen+f'{us}'+jsidra+']')
+                with open('Sajed.txt', 'a') as x:
+                	x.write(us+ '\n')
+                	tlg ='https://api.telegram.org/bot{}/sendMessage?chat_id={}&text=🔰User Insta Sajed tool🔰\n————————————\n .❖.User Instagram: {} \n————————————\n .❖.CH :- @v_xlj  .❖. :- @H_ils'.format(TK,ID,us)           	      	
+                	requests.post(tlg)            
+#Sidra elezz             
+            else:
+                Number+=1
+                print(jsidra+'['+BGreen+f'{Number}'+jsidra+'] Username not Live'+BGreen+'->'+jsidra+'['+BRed+f'{us}'+jsidra+']')
+
+if sidra =='2':
+	print('5')
+
+#Sidra elezz  	
+else:
+	print('')
+Code_sidra()           
